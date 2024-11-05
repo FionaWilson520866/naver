fd223d24d0f877d69f4ddb8da3e6a6b8a1a84f26
KALI <FionaWilson520866@hotmail.com>
2024-11-05 12:53:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "25548656")
     API_HASH = os.environ.get("API_HASH", "b9aacf719653fab4d0187ab904d253c7")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7524629229:AAG122GhmIDWhMx1RaT7WroeLmxVayrWhfg") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6984419962:AAHXVS7yjkBoqUS6z201zva6mSDmaEyzRGw") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://botzashu:LjkXI1JoztDQiQlr@cluster0.38ague0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "forward-bot")
